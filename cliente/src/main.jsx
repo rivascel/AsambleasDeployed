2be7687a41f8adce0052d4b60bdd3f6a39765a44
Ivan <rivascel@gmail.com>
2025-12-29 18:58:26 -0500

    react18

--- cliente/src/main.jsx
+++ cliente/src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './routes/App.jsx'
 import UserProvider from "./components/UserContext";
 
+console.log('React version:', React.version);
+console.log('ReactDOM version:', ReactDOM.version);
+
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <UserProvider>

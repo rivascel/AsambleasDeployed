ac02538a23527bfc06e9beb54f8b7a8e5e86f1a7
Ivan <rivascel@gmail.com>
2025-12-30 14:51:17 -0500

    ajuste gemini

--- cliente/src/routes/App.jsx
+++ cliente/src/routes/App.jsx
@@ -9,12 +9,14 @@ import AdminRegister from '../containers/admin/AdminRegister';
 import Home from '../pages/Home';
 import ProtectedRoute from '../containers/ProtectedRoute';
 import AppContext from '../context/AppContext';
+import UserProvider from "../components/UserContext";
 
 function App() {
   const initialState = useInitialState();	
 
   return (
     <AppContext.Provider value={initialState}>
+      <UserProvider>
         <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home/>}/> 
@@ -24,6 +26,7 @@ function App() {
               <Route path="/magic-link" element={<MagicLinkVerification />} />
             </Routes>
         </BrowserRouter>
+      </UserProvider>
     </AppContext.Provider>
   )
 }
